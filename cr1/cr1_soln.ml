f7afa0b5229a99273a34be282f1e10967ce0d7dc
jaydenpersonnat <89941534+jaydenpersonnat@users.noreply.github.com>
2024-02-02 17:12:00 -0500

    post solutions for code review 1

--- cr1/cr1_soln.ml
+++ cr1/cr1_soln.ml
@@ -3,29 +3,52 @@
 (* Define an infix operator +/. that computes the average of two-floating point 
    numbers *)
 
-let (+/.) x y = failwith "not yet implemented" ;; 
+let (+/.) (x : float) (y : float) : float =
+  (x +. y) /. 2. ;;
 
 
 (* Define a function fib such that fib n is the nth number in the Fibonacci
 sequence, which is 1, 1, 2, 3, 5, 8, 13... *)
 
-let fib n = failwith "not yet implemented" ;; 
+let rec fib (n : int) = 
+  if n = 1 || n = 2 then 1 
+  else fib (n - 1) + fib (n - 2) ;; 
 
 
 (* Define a function subset_sum that checks if there exists a subset of a
 list with a given sum (called the target). *)
-let subset_sum target lst = failwith "not yet implemented" ;; 
+let rec subset_sum (target: int) (lst : int list)  = 
+  match lst with 
+  | [] -> target == 0 
+  | h :: tl -> subset_sum target tl || subset_sum (target - h) tl ;; 
 
 
 (* Define a function partition that divides a list into two lists, such that the
 first list contains elements that satisfy a predicate function f and the second list contains elements
 that don’t satisfy the predicate. You may assume the input list is an integer list. *)
-let partition pred lst = failwith "not yet implemented" ;; 
+let rec partition (pred : int -> bool) (lst : int list) : int list * int list =
+  match lst with
+  | [] -> [], []
+  | h :: t ->
+    let y, n = partition pred t in
+    if pred h then h :: y, n
+    else y, h :: n ;;
 
 
-(* Define a function pack that packs consecutive dupli-
-cates of list elements into sublists. You may assume that the input lists are string lists. *)
-let pack lst = failwith "not yet implemented" ;; 
+(* Define a function pack that packs consecutive duplicates of list elements into sublists. 
+   You may assume that the input lists are string lists. *)
+let rec pack (lst : string list) : string list list =
+  match lst with
+  | [] -> []
+  | h :: t ->
+    match pack t with
+    | [] -> [[h]]
+    | hd :: tl as l ->
+      match hd with
+      | [] -> []
+      | h1 :: _ ->
+        if h1 = h then (h :: hd) :: tl
+        else [h] :: l ;;
 
 
 (* 
@@ -41,25 +64,47 @@ board. What type expression is appropriate to represent a piece’s position? (n
 a placeholder below so that this will compile)
 
 *)
-type position = int ;; 
+type position = char * int ;; 
 
 
 (* After defining position, define a function count_position that takes in a position and
 a position list, and counts the number of times the position occurs in the position
 list. *)
 
-let count_position pos lst = failwith "not yet implemented" ;; 
+let rec count_position (pos : position) (lst : position list) : int =
+  match lst with
+  | [] -> 0
+  | h :: t -> (if h = pos then 1 else 0) + count_position pos t ;;
 
 
 (* A knight in chess can move two squares vertically and one square horizontally, or two squares
 horizontally and one square vertically (in an "L" shape). Define a function legal_knight_moves
 that takes in a position and returns a position list representing all the possible posi-
 tions the knight can move to. Hint: You may find the Char module helpful here. *)
-let legal_knight_moves position = failwith "not yet implemented" ;; 
+let legal_knight_moves ((c, r) : position) : position list =
+  let possible_moves = 
+    [(2, 1); (2, -1); (-2, 1); (-2, -1); (1,2); (-1, 2); (-1, -2); (1, -2)] in 
+  let in_bounds ((c, i) : position) : bool = 
+    c >= 'a' && c <= 'h' && i >= 1 && i <= 8 in 
+  let rec aux (lst: (int * int) list) : position list = 
+    match lst with 
+    | [] -> [] 
+    | (h, v) :: t -> 
+      let pros_move = (Char.chr (Char.code c + h), r + v) in 
+      if in_bounds pros_move then pros_move :: aux t else aux t 
+    in aux possible_moves ;; 
+   
 
 
 (* Define a function min_knight_steps that takes in a starting position s and a target
 position t, and returns the minimum number of moves it takes for a knight to go from s or 6
 t. Note, that a knight can visit any position on the board from any starting position, so there’s
 always a solution! See the Knight’s tour. You may use the List.map function here. *)
-let min_knight_steps s t = failwith "not yet implemented" ;; 
+let rec min_knight_steps (s : position) (t : position) : int =
+  let rec min_list (lst : int list) : int = 
+    match lst with 
+    | [] -> -1 
+    | [elt] -> elt 
+    | h :: t -> min h (min_list t) in 
+  if s = t then 0 
+  else 1 + min_list (List.map (fun x -> min_knight_steps x t) (legal_knight_moves s)) ;; 
