94c97ab2a97cffaf679a92d348037be6a713c3bd
jaydenpersonnat <89941534+jaydenpersonnat@users.noreply.github.com>
2024-03-22 14:46:08 -0400

    update cr6

--- cr6/cr6.ml
+++ cr6/cr6.ml
@@ -8,6 +8,19 @@ so on. In general, the function should return n! on the nth time it is called.
 let mem_factorial _ = failwith "not implemented" ;;
 
 
+(* Define a non-recursive implementation of List.filter *)
+let filter_imper _ = failwith "not implemented" ;; 
+
+(* range n m returns a list that contains all the integers from n to m-1 inclusive. Define three
+versions of the range function: (1) a tail-recursive version, (2) a non-tail recursive version,
+and (3) a procedural implementation (using loops) *)
+
+let range _ = failwith "not implemented" ;; 
+
+(* Define a function that merges two sorted (in ascending order) mutable lists. This function
+should return a value of type unit and the first list should become the merging of the two lists *)
+let merge _ = failwith "not implemented" ;; 
+
 (* Define a function that generates the Fibonacci numbers (i.e. fib(i) the ith time the function
 is called *)
 let fib _ = failwith "not implemented" ;;
