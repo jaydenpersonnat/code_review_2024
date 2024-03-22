53eb7f0b5ccec2dd3723fd1af0a2a0aab3859e42
jaydenpersonnat <89941534+jaydenpersonnat@users.noreply.github.com>
2024-03-22 08:52:53 -0400

    post cr6

---
+++ cr6/cr6.ml
@@ -0,0 +1,18 @@
+
+(* 
+Define a function mem_factorial which takes a unit as an argument and returns 1 the first
+time it is called, returns 2 the second time it is called, returns 6 the third time it is called, and
+so on. In general, the function should return n! on the nth time it is called. 
+*)
+
+let mem_factorial _ = failwith "not implemented" ;;
+
+
+(* Define a function that generates the Fibonacci numbers (i.e. fib(i) the ith time the function
+is called *)
+let fib _ = failwith "not implemented" ;;
+
+
+(* Rewrite power from Part 2 (see Problem 6) to improve the worst-case time complexity. 
+What is the asymptotic runtime of the function right now? *)
+let rec power _ = failwith "not implemented" ;;
